0614f2ec49354655a7889233fc9ab7d98b887108
Murilo Vasconcelos <muriloime@gmail.com>
2022-04-05 19:36:31 -0300

    add js to cache_sweeper

--- lib/marcador/engine.rb
+++ lib/marcador/engine.rb
@@ -4,6 +4,7 @@ module Marcador
 
     initializer 'marcador.importmap', before: 'importmap' do |app|
       app.config.importmap.paths << Engine.root.join('config/importmap.rb')
+      app.config.importmap.cache_sweepers << Engine.root.join('app/assets/javascripts')
     end
 
     initializer :append_migrations do |app|
