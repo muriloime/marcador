37518b1a88e307c57425b059041ea7879125c3a7
Murilo Vasconcelos <muriloime@gmail.com>
2022-04-04 23:11:49 -0300

    rubocop

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,13 +1,10 @@
-
-pin "@hotwired/stimulus", to: "stimulus.min.js" , preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "rangy", to: "https://ga.jspm.io/npm:rangy@1.3.0/lib/rangy-core.js"
-pin "rangy-classapplier", to: "https://ga.jspm.io/npm:rangy@1.3.0/lib/rangy-classapplier.js"
-pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.6/src/index.js"
+pin 'rangy', to: 'https://ga.jspm.io/npm:rangy@1.3.0/lib/rangy-core.js'
+pin 'rangy-classapplier', to: 'https://ga.jspm.io/npm:rangy@1.3.0/lib/rangy-classapplier.js'
+pin '@rails/request.js', to: 'https://ga.jspm.io/npm:@rails/request.js@0.0.6/src/index.js'
 
 # marcador entrypoint
 # pin "marcador/application", preload: true
-pin "application", to: "marcador/application.js", preload: true
+pin 'application', to: 'marcador/application.js', preload: true
 
-
-pin_all_from Marcador::Engine.root.join("app/assets/javascripts/marcador/controllers"), under: "controllers", to: "marcador/controllers"
+pin_all_from Marcador::Engine.root.join('app/assets/javascripts/marcador/controllers'), under: 'controllers',
+                                                                                        to: 'marcador/controllers'
--- lib/marcador/engine.rb
+++ lib/marcador/engine.rb
@@ -2,10 +2,9 @@ module Marcador
   class Engine < ::Rails::Engine
     isolate_namespace Marcador
 
-    initializer "marcador.importmap", before: "importmap" do |app|
-      app.config.importmap.paths << Engine.root.join("config/importmap.rb")
+    initializer 'marcador.importmap', before: 'importmap' do |app|
+      app.config.importmap.paths << Engine.root.join('config/importmap.rb')
     end
-
 
     initializer :append_migrations do |app|
       unless app.root.to_s.match?(root.to_s)
