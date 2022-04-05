2defc407458ad174c261d1c753d552322decc90f
Murilo Vasconcelos <muriloime@gmail.com>
2022-04-05 09:27:43 -0300

    fix enrich_highlights, termporarily add class_name ( TODO: set as a config)

--- app/models/marcador/highlight.rb
+++ app/models/marcador/highlight.rb
@@ -1,16 +1,16 @@
 module Marcador
   class Highlight < ApplicationRecord
     belongs_to :highlightable, polymorphic: true
-    belongs_to :user
+    belongs_to :user, class_name: 'Ff::User'
 
-    def self.can_add_highlights?(highlightable, user)
-      return true # e.g. highlightable.can_highlight?(user)
+    def self.can_add_highlights?(_highlightable, _user)
+      true # e.g. highlightable.can_highlight?(user)
     end
 
     def self.enrich_highlights(user)
-      self.joins(:user)
-          .select("marcador_highlights.id as identifier,
-                  CONCAT('Highlight by ', users.id) as description,
+      joins(:user)
+        .select("marcador_highlights.id as identifier,
+                  CONCAT('Highlight by ', user_id) as description,
                   ARRAY[CASE user_id WHEN #{user.id} THEN 'me' ELSE 'others' END] as life_time_class_ends,
                   CASE user_id WHEN #{user.id} THEN true ELSE false END as can_cancel,
                   content,
